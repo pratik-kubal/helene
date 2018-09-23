21fcd635ca65ff173c26127e99df1e63c9f62902
pratik-kubal <17088230+pratik-kubal@users.noreply.github.com>
2018-09-23 19:50:10 -0400

    Added filter mat multiplication

--- edgeDetection.py
+++ edgeDetection.py
@@ -11,17 +11,31 @@ import cv2
 def rgbToGrayscale(image):
     # https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
     r,g,b = image[:,:,0],image[:,:,1],image[:,:,2]
-    grayscaleImage =  0.299*r + 0.587*g + 0.114* b
+    grayscaleImage =  0.299*r + 0.587*g + 0.114*b
     # Normalize Grayscale
     grayscaleImage = grayscaleImage / 255
     return grayscaleImage
 
-a = cv2.imread("proj1_cse573/task1.png")
-
-
+a = cv2.imread("./proj1_cse573/task1.png")
 b = rgbToGrayscale(a)
-print(b)
-type(b)
+
+def matMult(matA,matB):
+    matResult = 0
+    for x_iter in range(0,3):
+        for y_iter in range(0,3):
+            matResult += sobel_op_x[x_iter][y_iter] * matB[x_iter][y_iter]
+    return matResult
+
+sobel_op_x = [[1,0,-1],
+              [2,0,-2],
+              [1,0,-1]]
+sobel_op_y = [[1,2,1],
+              [0,0,0],
+              [-1,-2,-1]]
+
+for window_w in range(0,600):
+    for window_h in range(0,900):
+        window = b[0:3,0:3]
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow('image', b)
